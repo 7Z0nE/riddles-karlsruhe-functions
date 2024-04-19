e57a2b6ffa31e5932eb64b05a83b1802f5ae46d0
Frederik Wegner <onlyknoppas@gmail.com>
2024-04-19 11:05:49 +0200

    password8.js

--- functions/password8.js
+++ functions/password8.js
@@ -12,8 +12,17 @@ exports.handler = async (event, context) => {
           <title>Password Prompt</title>
         </head>
         <body>
-	  <p>Ein Blick in die Vergangenheit gibt dir den Hinweis!</p>
           <form action="/.netlify/functions/passwordChecker" method="post">
+	    <p>
+Im Nebel der Physik, wo Kräfte verborgen und Geheimnisse sich verbergen,
+Dort liegt ein Grundsatz, der das Universum in seiner Essenz lenkt.
+Wo Bewegung erzeugt und Veränderung gesteuert wird,
+Dort ruht die Antwort verborgen, in einem Gesetz von ewiger Gültigkeit.
+</p><p>
+Suche nicht nach offensichtlichen Zeichen oder greifbaren Beweisen,
+Doch finde die Lösung dort, wo das Gleichgewicht bestimmt.
+Wo die Natur ihr Mysterium enthüllt und das Unmögliche wahr wird,
+In einem Labyrinth des Wissens, wo die ersten Sätze die Weisheit bergen.</p>
             <label for="password">Enter Password:</label>
             <input type="password" id="password" name="password">
             <button type="submit">Submit</button>
@@ -26,13 +35,13 @@ exports.handler = async (event, context) => {
     // Check the password on a POST request
     const params = new URLSearchParams(event.body);
     const password = params.get('password');
-    const validPasswords = ['Sphynx', 'Sphynx Katze'];
+    const validPasswords = ['Energieerhaltung', 'Energieerhaltungssatz', 'Gesetz der Energieerhaltung', 'Energieerhaltungsgesetz'];
 
     if (validPasswords.includes(password)) {
       return {
         statusCode: 200,
         headers: { 'Content-Type': 'text/html' },
-        body: '<h1>Success: Password accepted!</h1><a href="https://docs.google.com/document/d/10Cgac8BOt5RpOxNu4J3BlfdZHL6zcEtThLFQ-03xq0k">Hinweis</a>'
+        body: '<h1>Success: Password accepted!</h1><a href="https://docs.google.com/document/d/1wRohjP2jsK4X_A0TEpsVo-sctHFAZBuWdJ44D6s1R8c">Hinweis</a>'
       };
     } else {
       return {
